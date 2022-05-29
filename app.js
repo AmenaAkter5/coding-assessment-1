2f44a1cdff986b13a9ed8987e92646371679eb59
unknown <latapata1952@gmail.com>
2022-05-30 01:30:53 +0600

    add row is good

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('table');
+const addRowBottom = () => {
+    const table = document.getElementById("table");
+    const newRow = table.insertRow(-1);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "6";
+    cell2.innerHTML = "NEW CELL2";
+}
+
+const addRowTop = () => {
+    const table = document.getElementById("table");
+    const newRow = table.insertRow(1);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "1";
+    cell2.innerHTML = "NEW CELL2";
+}
